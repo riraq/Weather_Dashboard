c422a9eb038700f0d060267b12075047e4f04a53
riraq <riraq@mail.usf.edu>
2021-01-29 16:38:12 -0600

    added functionality so that the weather stats for a searched city appear on the page

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,51 @@
-var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=6d57a13744a578704f3fd16ba8940763"
+var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q="
 
 // API Key - 6d57a13744a578704f3fd16ba8940763
+
+var searchBtn = document.querySelector("#searchBtn")
+
+// function for search button
+searchBtn.addEventListener("click", function(event){
+    event.preventDefault();
+
+    
+    var searchInput = document.querySelector("#searchInput")
+    var cityWeatherURL = weatherURL + searchInput.value + "&units=imperial&appid=6d57a13744a578704f3fd16ba8940763"
+    
+    fetch(cityWeatherURL)
+    .then(function (response) {
+        //   if (!response.ok) {
+        //     throw response.json();
+        //   }
+
+        return response.json();
+    })
+
+    .then(function(data){
+        console.log(data)
+        var cityName = document.querySelector("#city");
+        var cityTemperature = document.querySelector("#temperature");
+        var cityHumidity = document.querySelector("#humidity");
+        var cityWindspeed = document.querySelector("#windSpeed");
+        var date = new Date().toLocaleDateString()
+
+        cityName.textContent = data.name + " (" + date + ")";
+        cityTemperature.textContent = "Temperature: " + data.main.temp + "°F";
+        cityHumidity.textContent = "Humidity: " + data.main.humidity + " %";
+        cityWindspeed.textContent = "Wind Speed: " + data.wind.speed  + " MPH";
+        
+        var cityLon = data.coord.lon;
+        var cityLat = data.coord.lat;
+        var uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&appid=6d57a13744a578704f3fd16ba8940763"
+        
+        fetch(uvIndexURL)
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function(data){
+            var cityUvIndex = document.querySelector("#uvIndex");
+            cityUvIndex.textContent = "UV Index: " + data.value;
+    })
+    })
+})
